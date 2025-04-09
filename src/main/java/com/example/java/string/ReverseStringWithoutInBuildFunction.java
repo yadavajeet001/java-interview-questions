18c2b31b68d4628368f52c259348b515bd23af29
Ajeet Yadav <yadavajeet837@gmail.com>
2025-04-09 08:17:11 +0530

    added ReverseStringWithoutInBuildFunction solution

---
+++ src/main/java/com/example/java/string/ReverseStringWithoutInBuildFunction.java
@@ -0,0 +1,19 @@
+package com.example.java.string;
+
+//Reverse a given string  without using inbuilt function
+public class ReverseStringWithoutInBuildFunction {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        String result = reverseString(str);
+        System.out.println(result);
+    }
+
+    private static String reverseString(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            stringBuilder.append(str.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+}
