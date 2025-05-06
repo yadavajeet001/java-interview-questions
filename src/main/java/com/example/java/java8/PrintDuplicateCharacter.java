39af717981e67018c3f61b159f5c31d89ab25f5e
Ajeet Yadav <yadavajeet837@gmail.com>
2025-05-06 09:38:26 +0530

    added example for PrintDuplicateCharacter in java 8

---
+++ src/main/java/com/example/java/java8/PrintDuplicateCharacter.java
@@ -0,0 +1,15 @@
+package com.example.java.java8;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+//print the duplicate character
+public class PrintDuplicateCharacter {
+    public static void main(String[] args) {
+        String name = "ajeet";
+        name.chars()
+                .mapToObj(ch -> (char) ch).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(entry -> entry.getValue() > 1)
+                .forEach((res -> System.out.println("duplicate characters :" + res)));
+    }
+}
