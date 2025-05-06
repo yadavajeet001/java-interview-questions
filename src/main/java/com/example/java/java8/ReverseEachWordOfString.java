f328069cc07f01627d305da308304403ec5e22d5
Ajeet Yadav <yadavajeet837@gmail.com>
2025-05-06 10:14:47 +0530

    added example for ReverseEachWordOfString in java 8

--- src/main/java/com/example/java/java8/ReverseEachWordOfString.java
+++ src/main/java/com/example/java/java8/ReverseEachWordOfString.java
@@ -14,6 +14,5 @@ public class ReverseEachWordOfString {
                 .map(word -> new StringBuilder(word).reverse())
                 .collect(Collectors.joining(" "));
         System.out.println("Reversed String : " + result);
-
     }
 }
