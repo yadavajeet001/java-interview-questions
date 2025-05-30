9da5252dc24200d9f1fa6416b41f37032f58d24c
Ajeet Yadav <yadavajeet837@gmail.com>
2025-05-30 13:32:39 +0530

    added example for frequency of characters

--- src/main/java/com/example/java/java8/CountFrequencyOfChars.java
+++ src/main/java/com/example/java/java8/CountFrequencyOfChars.java
@@ -10,15 +10,23 @@ public class CountFrequencyOfChars {
 
         String names = "Ajay Ajeet Chandan";
 
+        System.out.println("-------count frequency of repeating characters-----");
         Map<Character, Long> map = names.chars().filter(c -> c != ' ')
                 .mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        map.forEach((character, count) -> System.out.println(character + "->" +count));
+        map.forEach((character, count) -> System.out.println(character + "->" + count));
+
+        System.out.println("-------count frequency of only repeating characters-----");
+        Map<Character, Long> repeatedCharCount = names.chars().filter(c -> c != ' ')
+                .mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        repeatedCharCount.entrySet().stream().filter(entry -> entry.getValue() > 1).forEach((System.out::println));
 
         String names2 = "Ajay, Ajeet, Chandan";
-        System.out.println("-------------second way------------");
+        System.out.println("-------------second way to count frequency of all character------------");
         Map<Character, Long> frequencyCount = names2.chars().map(Character::toLowerCase).filter(c -> c != ',' && c != ' ')
                 .mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        frequencyCount.forEach((character, aLong) -> System.out.println(character + "->" +aLong));
+        frequencyCount.forEach((character, aLong) -> System.out.println(character + "->" + aLong));
+
 
     }
 }
