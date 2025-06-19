33419e5cbe934cfbce89147610b85338089a8909
Ajeet Yadav <yadavajeet837@gmail.com>
2025-06-19 21:22:20 +0530

    modified example for longest substring

--- src/main/java/com/example/java/string/LongestSubStringLengthWithoutRepeatingChar.java
+++ src/main/java/com/example/java/string/LongestSubStringLengthWithoutRepeatingChar.java
@@ -1,14 +1,17 @@
 package com.example.java.string;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 //Find the longest substring length without repeating character in a given string
 public class LongestSubStringLengthWithoutRepeatingChar {
 
     public static void main(String[] args) {
         String str = "abcabcaa";
-        System.out.println("Longest subString length : " + findLongestSubStringLengthWithoutRepeatingChar(str));
+        System.out.println("Longest subString length1 : " + findLongestSubStringLengthWithoutRepeatingChar(str));
+        System.out.println("Longest substring length2 : " + lengthOfLongestSubstring(str));
     }
 
     private static int findLongestSubStringLengthWithoutRepeatingChar(String str) {
@@ -31,4 +34,24 @@ public class LongestSubStringLengthWithoutRepeatingChar {
         }
         return maxLength;
     }
+
+    //Time Complexity - O(n) as right traverse loop once and left traverse less than right
+    //Space complexity - O(1) for fixed character and O(n) for variable or large input
+    //sliding window pattern
+    public static int lengthOfLongestSubstring(String s) {
+
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0, max = 0;
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right++));
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(left++));
+            }
+        }
+        return max;
+    }
+
+
 }
