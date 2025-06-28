6b71d7016f5f177fcca4af915142d2a6134fd294
Ajeet Yadav <yadavajeet837@gmail.com>
2025-06-28 13:30:29 +0530

    added example for Find Second-Largest Element in array

--- src/main/java/com/example/java/array/FindSecondLargestElement.java
+++ src/main/java/com/example/java/array/FindSecondLargestElement.java
@@ -4,7 +4,7 @@ public class FindSecondLargestElement {
 
     public static void main(String[] args) {
 
-        int[] arr = {12, 34, 3, 5, 35};
+        int[] arr = {12, 36, 3, 5, 35};
 
         int largest = 0, secondLargest = 0;
 
@@ -12,6 +12,8 @@ public class FindSecondLargestElement {
             if (num > largest) {
                 secondLargest = largest;
                 largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
             }
         }
         System.out.println(secondLargest);
