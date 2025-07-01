397f2d84092d91490de128dec6a6360b7194d715
Ajeet Yadav <yadavajeet837@gmail.com>
2025-07-01 18:27:36 +0530

    Added code of FirstNonRepeatingCharacter problem

---
+++ src/main/java/com/example/java/java8/FirstNonRepeatingCharacter.java
@@ -0,0 +1,34 @@
+package com.example.java.java8;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String name = "Aajeet";
+        Character character = name.chars().mapToObj(ch -> (char) ch)
+                .collect(Collectors.toMap(Character::charValue, v -> 1, Integer::sum, LinkedHashMap::new))
+                .entrySet().stream().filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(null);
+
+        if (character != null) {
+            System.out.println("First non repeating character : " + character);
+        } else {
+            System.out.println("No non repeating character");
+        }
+
+        //Another way
+        Character firstNonRepeatingChar = name.chars().mapToObj(ch -> (char) ch).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream().filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey).findFirst().orElse(null);
+
+        if (firstNonRepeatingChar != null) {
+            System.out.println("First non repeating character: " + firstNonRepeatingChar);
+        } else {
+            System.out.println("No non repeating character");
+        }
+    }
+}
