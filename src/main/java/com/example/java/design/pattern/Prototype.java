022c2b1d1d3df75501b894ebea3a0187246a09ac
Ajeet Yadav <yadavajeet837@gmail.com>
2025-07-15 16:53:32 +0530

    added example for Prototype

---
+++ src/main/java/com/example/java/design/pattern/Prototype.java
@@ -0,0 +1,9 @@
+package com.example.java.design.pattern;
+
+/**
+ * The Prototype Design Pattern is a creational pattern that enables the creation of new objects by
+ * copying an existing object. Prototype allows us to hide the complexity of making new instances
+ * from the client. The existing object acts as a prototype and contains the state of the object.
+ */
+public class Prototype {
+}
